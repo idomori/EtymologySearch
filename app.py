db3449be94d876f4d2274b05b5ac6464a2e60d03
idomori <ftk@umich.edu>
2025-05-22 14:58:57 +0800

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
 from indexer.search_index import search_index
-from indexer.search_index import search_index
 from indexer.graph_builder import build_clean_etymology_graph
-
+from indexer.llm_parser import extract_etymology_graph
 
 app = Flask(__name__)
 
@@ -13,12 +12,17 @@ def home():
 
 @app.route('/search')
 def search():
-    query   = request.args.get('q', '')
-    results = search_index(query)
+    query = request.args.get('q', '')
+
+    #etym_text = get_etym_text(query)
+    query_info, results = search_index(query)
+    print(extract_etymology_graph(query_info))
+    
+    
 
     # build the live graph
     graph_data = build_clean_etymology_graph(query)
-
+    #print(graph_data)
     return render_template(
         'search_results.html',
         query=query,
